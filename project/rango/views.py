335b5abb0b71916c89623404e21dd474ab88a0a3
Denis <d.iskandarov@gmail.com>
2016-03-25 17:06:13 +0400

    ch10: Working with templates

--- project/rango/views.py
+++ project/rango/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
